ed71c85eb8c9973d532d43cb8bf6dbfdb7f2b47d
Codemis <johnathan@jpulos.com>
2016-03-22 22:28:15 -0700

    Added several validations for registering a consumer.

--- routes/consumers.js
+++ routes/consumers.js
@@ -3,30 +3,48 @@
  */
 'use strict';
 
+/**
+ * Get the objectIsEmpty Utility
+ *
+ * @param {Object}
+ */
+var isObjectEmpty = require('../utilities/is-object-empty');
+
 module.exports = function (restify, server, models) {
 
   server.post('/consumers/register', function(req, res) {
-    var clientId = req.headers['x-client-id'];
-    var consumerData = models.Consumer.parseRequest(req.params);
-    models.Client.findOne({
-      where: { applicationId: clientId }
-    })
-    .then(function(client) {
-      if (client) {
-        /**
-         * register the client
-         */
-        models.Consumer.create(consumerData)
-        .then(function(consumer) {
-          res.header('x-api-key', consumer.apiKey);
-          res.send(201, consumer.toResponse());
-        });
-      } else {
-        /**
-         * Client does not exist
-         */
-      }
-    });
+    var params = req.params;
+    if ((!req.headers.hasOwnProperty('x-client-id')) || (req.headers['x-client-id'] === '')) {
+      res.header('x-api-key', '');
+      res.send(400, { 'error': 'Not Found. The client could not be found on the server.' });
+    } else if ((isObjectEmpty(params)) || (isObjectEmpty(params.device)) || (isObjectEmpty(params.push))) {
+      res.header('x-api-key', '');
+      res.send(400, { 'error': 'Bad Request. The data you provided is malformed or missing.' });
+    } else {
+      var clientId = req.headers['x-client-id'];
+      var consumerData = models.Consumer.parseRequest(params);
+      models.Client.findOne({
+        where: { applicationId: clientId }
+      })
+      .then(function(client) {
+        if (client) {
+          /**
+           * register the client
+           */
+          models.Consumer.create(consumerData)
+          .then(function(consumer) {
+            res.header('x-api-key', consumer.apiKey);
+            res.send(201, consumer.toResponse());
+          });
+        } else {
+          /**
+           * Client does not exist
+           */
+          res.header('x-api-key', '');
+          res.send(400, { 'error': 'Not Found. The client could not be found on the server.' });
+        }
+      });
+    }
   });
 
 };
--- tests/integration/routes-consumers.test.js
+++ tests/integration/routes-consumers.test.js
@@ -8,35 +8,73 @@ describe('Routes Consumers', function () {
   });
 
   describe('/consumers/register', function () {
+    var data = {
+      "device": {
+        "model": "Nexus 7",
+        "platform": "Android",
+        "uuid": "03bb8569-7fb9-4883-9779-56c6651d694c",
+        "version": "4.4"
+      },
+      "push": {
+        "interval": 20000,
+        "receive": true,
+        "timezone": "America/Los_Angeles",
+        "token": "5e7a72d6-d076-11e5-ab30-625662870761"
+      }
+    };
 
     it('should register a new consumer', function (done) {
-      data = {
-        "device": {
-          "model": "Nexus 7",
-          "platform": "Android",
-          "uuid": "03bb8569-7fb9-4883-9779-56c6651d694c",
-          "version": "4.4"
-        },
-        "push": {
-          "interval": 20000,
-          "receive": true,
-          "timezone": "America/Los_Angeles",
-          "token": "5e7a72d6-d076-11e5-ab30-625662870761"
-        }
-      };
       api.post('/consumers/register')
-        .send(data)
-        .set('Accept', 'application/json')
-        .set('X-Client-Id', clientAppId)
-        .end(function(err, res) {
-          expect(res.ok).to.be.true;
-          expect(res.status).to.equal(201);
-          expect(res.headers.hasOwnProperty('x-api-key')).to.be.true;
-          expect(res.headers['x-api-key']).not.to.equal('null');
-          expect(res.body.hasOwnProperty('createdAt')).to.be.true;
-          done();
-        });
+      .send(data)
+      .set('Accept', 'application/json')
+      .set('x-client-id', clientAppId)
+      .end(function(err, res) {
+        expect(res.ok).to.be.true;
+        expect(res.status).to.equal(201);
+        expect(res.headers.hasOwnProperty('x-api-key')).to.be.true;
+        expect(res.headers['x-api-key']).not.to.equal('null');
+        expect(res.body.hasOwnProperty('createdAt')).to.be.true;
+        done();
       });
+    });
+
+    it('should require the client id', function (done) {
+      api.post('/consumers/register')
+      .send(data)
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        expect(res.status).to.equal(400);
+        expect(res.body.hasOwnProperty('error')).to.be.true;
+        expect(res.body.error.match(/client could not be found/g)).to.not.equal(null);
+        done();
+      });
+    });
+
+    it('should require a valid client id', function (done) {
+      api.post('/consumers/register')
+      .send(data)
+      .set('Accept', 'application/json')
+      .set('x-client-id', 'MADEUP#ID231')
+      .end(function(err, res) {
+        expect(res.status).to.equal(400);
+        expect(res.body.hasOwnProperty('error')).to.be.true;
+        expect(res.body.error.match(/client could not be found/g)).to.not.equal(null);
+        done();
+      });
+    });
+
+    it('should require the consumer data', function (done) {
+      api.post('/consumers/register')
+      .send({})
+      .set('Accept', 'application/json')
+      .set('x-client-id', clientAppId)
+      .end(function(err, res) {
+        expect(res.status).to.equal(400);
+        expect(res.body.hasOwnProperty('error')).to.be.true;
+        expect(res.body.error.match(/malformed or missing/g)).to.not.equal(null);
+        done();
+      });
+    });
 
   });
 
